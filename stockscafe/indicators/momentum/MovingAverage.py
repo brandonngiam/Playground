ee438db53f12bdf51d6fed2cf2cfe9f4870e976e
evankoh <ekoh@indeed.com>
2019-03-10 11:25:27 +0900

    Refactor MovingAverage

--- stockscafe/indicators/momentum/MovingAverage.py
+++ stockscafe/indicators/momentum/MovingAverage.py
@@ -1,20 +1,21 @@
 from ...utils import Ordering
 import numpy as np
 
-def compute(df, lookback):
+def compute(df, window):
     df = Ordering.byDate(df, True) # Need dates to be ascending order
-    ma = df['close'].rolling(lookback).mean()
-    df[f'sma{lookback}'] = ma
+    df[f'sma{window}'] = computeSmaGivenList(df['close'], window)
     return df
 
-def computeExponential(df, lookback):
+def computeExponential(df, window):
     df = Ordering.byDate(df, True) # Need dates to be ascending order
-    ema = numpy_ewma_vectorized_v2(df['close'], lookback)
-    df[f'ema{lookback}'] = ema
+    df[f'ema{window}'] = computeEmaGivenList(df['close'], window)
     return df
+
+def computeSmaGivenList(data, window):
+    return data.rolling(window).mean()
 
 # https://stackoverflow.com/questions/42869495/numpy-version-of-exponential-weighted-moving-average-equivalent-to-pandas-ewm
-def numpy_ewma_vectorized_v2(data, window):
+def computeEmaGivenList(data, window):
     alpha = 2 /(window + 1.0)
     alpha_rev = 1-alpha
     n = data.shape[0]
@@ -28,6 +29,6 @@ def numpy_ewma_vectorized_v2(data, window):
     mult = data*pw0*scale_arr
     cumsums = mult.cumsum()
     out = offset + cumsums*scale_arr[::-1]
-    for i in range(int(window + window/2)): # first X elements are not stable
+    for i in range(int(window + window/2)): # first (window + window/2) elements are not stable
         out[i] = np.nan
     return out
