e8bfa4e9e616331a493f65c16d0a1844104245f9
Joe Ang <joemeister@gmail.com>
2019-04-08 21:29:16 +0800

    New features for StocksCafeApi.py
    - Changed getResult() to return more attributes
      such as total pages.
    
    - Updated tutorials to reflect new API results format
      - Check Close % from 52-Week Low.ipynb
      - List of available indicators.ipynb
      - List of StocksCafe API.ipynb
      - Monthly Dividends Collected Graphs.ipynb
      - Plot Moving Average Chart.ipynb
    
    - Move endpoint URLs into individual methods.
      This avoids build up of class variables that are not
      accessed by multiple methods.
    
    - Add page support for getPrices().
    
    - Add getPortfolioTransactions().
    
    - Break up long code to fit 79 chars.

--- stockscafe/StocksCafeApi.py
+++ stockscafe/StocksCafeApi.py
@@ -3,9 +3,6 @@ from pandas.io.json import json_normalize
 
 class StocksCafeApi(object):
     domain = 'https://api.stocks.cafe'
-    recentPricesUrl = f'{domain}/stock.json?l=recent_prices'
-    apiCountUrl = f'{domain}/user.json?l=count'
-    collectedDividendsUrl = f'{domain}/portfolio.json?l=collected_dividends'
 
     def __init__(self, api_key_file='api-key.txt', apiUser='', apiUserKey=''):
         if apiUser != '' or apiUserKey != '':
@@ -13,29 +10,44 @@ class StocksCafeApi(object):
             self.apiUserKey = apiUserKey
         else:
             f = open(api_key_file, "r") 
-            self.apiUser = f.readline().rstrip() # First line shall be api_user
-            self.apiUserKey = f.readline().rstrip() # Second line shall be api_user_key
+            # First line shall be api_user
+            self.apiUser = f.readline().rstrip()
+            # Second line shall be api_user_key
+            self.apiUserKey = f.readline().rstrip()
 
     def getCreds(self):
         return f'api_user={self.apiUser}&api_user_key={self.apiUserKey}'
 
-    def getResult(self, url, result_key): # result_key is where the data sits
+    # result_key is where the main bulk of data sits.
+    # E.g. for prices, result_key == 'eod_list'
+    def getResult(self, url, result_key):
         json = requests.get(url).json()
         if json['result_boolean']:
             array = json[result_key]
-            return json_normalize(array)
+            # Convert result_key to dataframe
+            json[result_key] = json_normalize(array)
+            return json
         else:
             raise Exception(json['result'])
 
     def getUsageCount(self):
-        url = f'{self.apiCountUrl}&{self.getCreds()}'
+        url = f'{self.domain}/user.json?l=count&{self.getCreds()}'
         r = requests.get(url)
         return json_normalize(r.json())
 
-    def getPrices(self, exchange, symbol, lookback): 
-        url = f'{self.recentPricesUrl}&exchange={exchange}&symbol={symbol}&{self.getCreds()}&lookback={lookback}'
+    def getPrices(self, exchange, symbol, lookback, page = 1): 
+        url = f'{self.domain}/stock.json?l=recent_prices'
+        url += f'&exchange={exchange}&symbol={symbol}'
+        url += f'&{self.getCreds()}&lookback={lookback}'
         return self.getResult(url, 'eod_list')
 
     def getCollectedDividends(self, startDate, endDate):
-        url = f'{self.collectedDividendsUrl}&{self.getCreds()}&start_date={startDate}&end_date={endDate}'
+        url = f'{self.domain}/portfolio.json?l=collected_dividends'
+        url += f'&{self.getCreds()}&start_date={startDate}&end_date={endDate}'
+        return self.getResult(url, 'data')
+
+    def getPortfolioTransactions(self, page = 1):
+        url = f'{self.domain}/portfolio.json?l=portfolio_transactions'
+        url += f'&page={page}'
+        url += f'&{self.getCreds()}'
         return self.getResult(url, 'data')
